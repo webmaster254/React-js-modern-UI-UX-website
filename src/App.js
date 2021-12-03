2a8e9f3cd5995110284defd95434a2b0a784868e
Joseph <joseph@localhost.localdomain>
2021-12-03 16:39:13 +0300

    created header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
-import {Footer,Blog,Possibility,Features,WhatGPT3,Header} from './containers';
-import {CTA,Brand,Navbar} from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 import './App.css';
 
-const App = () =>(
+const App = () => (
   <div className="App">
-  <div className="gradient__bg">
-  <Navbar />
-  <Header />
-  </div>
-
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
   </div>
 );
 
